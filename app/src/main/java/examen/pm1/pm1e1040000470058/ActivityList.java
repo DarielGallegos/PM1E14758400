bfced127f109a7db5106dab3368ace1f9253df26
isnaya-andrade <isnayaandrade@gmail.com>
2024-06-21 01:48:13 -0600

    create view
    Creacion de la vista de lista de contactos

---
+++ app/src/main/java/examen/pm1/pm1e1040000470058/ActivityList.java
@@ -0,0 +1,24 @@
+package examen.pm1.pm1e1040000470058;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ActivityList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_list);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
